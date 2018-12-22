1e81b952ac65323f4d4509dd698753e40928802b
keepangry <0@keepangry.com>
2018-12-22 11:59:35 +0800

    lsh

---
+++ locality_sensitive_hashing/MinHash.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 18-12-22 上午11:54
+# @Author  : yangsen
+# @Mail    : 0@keepangry.com
+# @File    : MinHash.py
+# @Software: PyCharm
+
+
+class MinHash(object):
+    def __init__(self):
+        pass
+
+
+
+if __name__ == "__main__":
+    minhash = MinHash()
