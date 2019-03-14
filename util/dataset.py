8d95f0c872ef36cd09b520bd7a7d482e1f46ec96
keepangry <0@keepangry.com>
2019-03-14 08:45:02 +0800

    softmax regression half

--- util/dataset.py
+++ util/dataset.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import OneHotEncoder
 
 def binary_iris(type=0, top_features=4, random_state=1):
     """
@@ -31,5 +31,28 @@ def binary_iris(type=0, top_features=4, random_state=1):
         X, y, test_size=0.25, random_state=random_state)
 
 
+def multi_iris(top_features=4, random_state=1, onehot=False):
+    """
+
+    :param top_features:
+    :param random_state:
+    :return:
+    """
+    iris = load_iris()
+    X = iris.data[:, :top_features]
+
+    if onehot:
+        y = iris.target.reshape(-1, 1)
+        enc = OneHotEncoder(dtype=int, categories='auto')
+        enc.fit(y)
+        y = enc.transform(y).toarray()
+    else:
+        y = iris.target
+    # split train test
+    return train_test_split(
+        X, y, test_size=0.25, random_state=random_state)
+
+
 if __name__ == "__main__":
-    X_train, X_test, y_train, y_test = binary_iris()
+    # X_train, X_test, y_train, y_test = binary_iris()
+    X_train, X_test, y_train, y_test = multi_iris()
